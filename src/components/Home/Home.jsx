7f47e0986f639057a39e3721a79b15f02bf4e077
Lancherix <lancherix.service@gmail.com>
2024-11-10 15:19:39 -0500

    navbar2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,9 @@
 import './Home.css';
-import img from '../ArtWork/4CBB2462-1760-4046-AE38-470D1B6FA26F_1_105_c.jpeg';
 
 function Home() {
   return (
     <div className="Home">
-        <img src={img} alt="" />
+        
     </div>
   );
 }
