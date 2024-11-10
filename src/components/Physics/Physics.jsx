085f2f1527b8cee1b94e70f6aca4519125e26615
Lancherix <lancherix.service@gmail.com>
2024-11-10 18:25:43 -0500

    physics

--- src/components/Physics/Physics.jsx
+++ src/components/Physics/Physics.jsx
@@ -1,6 +1,22 @@
 import './Physics.css';
 
+import physicsLogo from '../ArtWork/logo1.png';
+import physicsVideo from '../ArtWork/PhysicsHero.mp4';
+
 function Physics() {
+  return (
+    <div className="Physics">
+      <div className='Physics-hero'>
+      <video autoPlay loop muted className="Physics-heroVideo"><source src={physicsVideo} type="video/mp4" /></video>
+        <div className='Physics-heroCover'>
+          <img src={physicsLogo} alt="Lancherix Physics" className='Physics-heroLogo' />
+          <p>Discover the secrets and mysteries</p>
+          <p>hidden behind physics wonders.</p>
+          <button>Start Learning</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Physics;
