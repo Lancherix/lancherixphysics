3b23b3e77f45bd74b287a71a440c829acfadcfe4
Lancherix <lancherix.service@gmail.com>
2024-11-11 10:37:56 -0500

    physicsWonders

--- src/components/Physics/Physics.jsx
+++ src/components/Physics/Physics.jsx
@@ -1,13 +1,13 @@
 import './Physics.css';
 
-import physicsLogo from '../ArtWork/logo1.png';
+import physicsLogo from '../ArtWork/logo2.png';
 import physicsVideo from '../ArtWork/PhysicsHero.mp4';
 
 function Physics() {
   return (
     <div className="Physics">
       <div className='Physics-hero'>
-      <video autoPlay loop muted className="Physics-heroVideo"><source src={physicsVideo} type="video/mp4" /></video>
+        <video autoPlay loop muted className="Physics-heroVideo"><source src={physicsVideo} type="video/mp4" /></video>
         <div className='Physics-heroCover'>
           <img src={physicsLogo} alt="Lancherix Physics" className='Physics-heroLogo' />
           <p>Discover the secrets and mysteries</p>
