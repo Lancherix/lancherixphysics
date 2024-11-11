625cfde2cae3eb9c89c8ac933f8cf06c5ed8aee8
Lancherix <lancherix.service@gmail.com>
2024-11-11 11:10:55 -0500

    favicon

--- src/components/Physics/Physics.jsx
+++ src/components/Physics/Physics.jsx
@@ -10,8 +10,7 @@ function Physics() {
         <video autoPlay loop muted className="Physics-heroVideo"><source src={physicsVideo} type="video/mp4" /></video>
         <div className='Physics-heroCover'>
           <img src={physicsLogo} alt="Lancherix Physics" className='Physics-heroLogo' />
-          <p>Discover the secrets and mysteries</p>
-          <p>hidden behind physics wonders.</p>
+          <p>Discover the secrets and mysteries hidden behind physics wonders.</p>
           <button>Start Learning</button>
         </div>
       </div>
