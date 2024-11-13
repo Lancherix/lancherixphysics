9d7a437e217358e720aeb95e0fee45a1b682a58f
Lancherix <lancherix.service@gmail.com>
2024-11-12 19:40:49 -0500

    Lamine Yamal

--- src/components/Sports/Sports.jsx
+++ src/components/Sports/Sports.jsx
@@ -5,6 +5,7 @@ function Physics() {
     <div className="Sports">
       <div className='Sports-hero'>
         <div className='Sports-heroCover'>
+          <h1>Lamine Yamal the new legend.</h1>
         </div>
       </div>
     </div>
