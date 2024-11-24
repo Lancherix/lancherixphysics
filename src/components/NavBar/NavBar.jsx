70d004d5d9e3c873160b8f7afb27cf949e8829f8
Lancherix <lancherix.service@gmail.com>
2024-11-24 12:27:15 -0500

    okay?

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -67,11 +67,8 @@ function NavBar() {
                 <Link to="/physics">Physics</Link>
               </div>
               <div className="NavBar-Item">
-<<<<<<< HEAD
                 <Link to="/physics/option1">Quantities and units</Link>
-=======
                 <Link to="/physics/option1">Physical quantities and units</Link>
->>>>>>> f9a48dc9ee9c98a4a71d7f2e4c3e85b0a91226f3
                 <Link to="/physics/option2">Kinematics</Link>
                 <Link to="/physics/option3">Dynamics</Link>
                 <Link to="/physics/option3">Forces, density and pressure</Link>
